bd57be8928404b6f8b9fd88fc321671f06acba4f
pstso01 <soujanyaps@gmail.com>
2018-10-06 11:25:20 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -7,18 +7,9 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet} from 'react-native';
-import Home from './Home';
+import Home from './app/Home';
 
-const instructions = Platform.select({
-  ios: '\n' + 'Stay tuned!',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
   render() {
     return (
       <Home />
@@ -26,21 +17,3 @@ export default class App extends Component<Props> {
   }
 }
 
-const styles1 = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
